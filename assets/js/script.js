d77626627094094498bd30840b1941e516de45b8
patel.akash30@icloud.com <patel.akash30@icloud.com>
2022-11-14 14:36:26 -0500

    rest of js

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,45 +26,45 @@ function setColors() {
 }
 // function to set placeholders for the textboxes/areas
 function placeholderSetter() {
-  for (let i = 1; i <= 9; i++) {
-    $(`#input${i}`).val(localStorage.getItem(`input${i}`));
+    for (let i = 1; i <= 9; i++) {
+      $(`#input${i}`).val(localStorage.getItem(`input${i}`));
+    }
   }
-}
-// function to set event listeners
-function eventListeners() {
-  for (let i = 1; i <= 9; i++) {
-    console.log(i);
-    $(`#button-addon${i}`).click(function () {
-      let input = $(`#input${i}`).val();
-      console.log(input);
-      $(`#label${i}`).text(input);
-      localStorage.setItem(`input${i}`, input);
-    });
+  // function to set event listeners
+  function eventListeners() {
+    for (let i = 1; i <= 9; i++) {
+      console.log(i);
+      $(`#button-addon${i}`).click(function () {
+        let input = $(`#input${i}`).val();
+        console.log(input);
+        $(`#label${i}`).text(input);
+        localStorage.setItem(`input${i}`, input);
+      });
+    }
   }
-}
-function setCurrentDay() {
-  $("#currentDay").text(
-    "Today is " +
-      now.format("dddd, MMMM D, YYYY" + ` (${hour}:${minute}:${second})`)
-  ); // sets the date at the top
-}
-
-setCurrentDay();
-setColors();
-logger();
-placeholderSetter();
-eventListeners();
-
-// time interval to call the functions every second
-setInterval(function () {
-  now = dayjs();
-  now0 = now;
-  hour = now.hour();
-  thisHour0 = now0.hour();
-  minute = now.minute();
-  second = now.second();
+  function setCurrentDay() {
+    $("#currentDay").text(
+      "Today is " +
+        now.format("dddd, MMMM D, YYYY" + ` (${hour}:${minute}:${second})`)
+    ); // sets the date at the top
+  }
+  
   setCurrentDay();
   setColors();
   logger();
-  // placeholderSetter();
-}, 1000);
+  placeholderSetter();
+  eventListeners();
+  
+  // time interval to call the functions every second
+  setInterval(function () {
+    now = dayjs();
+    now0 = now;
+    hour = now.hour();
+    thisHour0 = now0.hour();
+    minute = now.minute();
+    second = now.second();
+    setCurrentDay();
+    setColors();
+    logger();
+    // placeholderSetter();
+  }, 1000);
